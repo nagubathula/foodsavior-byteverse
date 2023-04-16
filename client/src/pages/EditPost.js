15b73d2fda7d2a0e478aa8181b246e65795a2e77
Sahithya Kattamuri <aythihas2003@gmail.com>
2023-04-16 10:36:32 +0530

    frontend for post and edit (#1)

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -48,93 +48,186 @@ export default function EditPost() {
   }
 
   return (
-    <main>
-      <div className="mx-auto align-middle max-w-7xl py-6 sm:px-6 lg:px-8">
-        <form className="align-middle max-w-7xl" onSubmit={updatePost}>
-          <div className="mt-16 flex flex-col xl:w-2/6 lg:w-2/3 md:w-2/3 w-full">
-            <label
-              htmlFor="title"
-              className="pb-2 text-sm font-bold text-gray-800"
+    <form onSubmit={updatePost}>
+      <div class="absolute  inset-0 h-screen overflow-hidden bg-white">
+        <svg
+          class="absolute  opacity-50 top-0 left-[max(50%,25rem)] h-[64rem] w-[128rem] -translate-x-1/2 stroke-primary [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+              width="200"
+              height="200"
+              x="50%"
+              y="-1"
+              patternUnits="userSpaceOnUse"
             >
-              Title
-            </label>
-            <input
-              type="title"
-              placeholder={"Title"}
-              value={title}
-              onChange={(ev) => setTitle(ev.target.value)}
-              className="border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 bg-transparent placeholder-gray-500 text-gray-500 dark:text-gray-400"
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="100%" y="-1">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+              stroke-width="0"
             />
-          </div>
-
-          <div className="mt-8 flex flex-col xl:w-3/5 lg:w-1/2 md:w-1/2 w-full">
-            <label
-              htmlFor="description"
-              className="pb-2 text-sm font-bold text-gray-800"
-            >
-              Description
-            </label>
-            <input
-              type="summary"
-              className="bg-transparent border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 resize-none placeholder-gray-500 text-gray-500 dark:text-gray-400"
-              placeholder={"Summary"}
-              value={summary}
-              onChange={(ev) => setSummary(ev.target.value)}
-              rows={5}
-            />
-            <p className="w-full text-right text-xs pt-1 text-gray-500 dark:text-gray-400">
-              Character Limit: 200
-            </p>
-          </div>
-          <div className="mt-8 flex flex-col xl:w-3/5 lg:w-1/2 md:w-1/2 w-full">
-            <label
-              htmlFor="description"
-              className="pb-2 text-sm font-bold text-gray-800"
-            >
-              Category
-            </label>
-            <input
-              type="category"
-              className="bg-transparent border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 resize-none placeholder-gray-500 text-gray-500 dark:text-gray-400"
-              placeholder={"Category"}
-              value={category}
-              onChange={(ev) => setCategory(ev.target.value)}
-              rows={5}
-            />
-            <p className="w-full text-right text-xs pt-1 text-gray-500 dark:text-gray-400">
-              Character Limit: 200
-            </p>
-          </div>
-          <div className="mt-8 flex flex-col xl:w-3/5 lg:w-1/2 md:w-1/2 w-full">
-            <label
-              htmlFor="description"
-              className="pb-2 text-sm font-bold text-gray-800"
-            >
-              Insert Image
-            </label>
-            <input
-              class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-              type="file"
-              onChange={(ev) => setFiles(ev.target.files)}
-            />
-          </div>
-          <div className="mt-8 flex flex-col xl:w-3/5 lg:w-1/2 md:w-1/2 w-full">
-            <label
-              htmlFor="description"
-              className="pb-2 text-sm font-bold text-gray-800"
-            >
-              Edit Content
-            </label>
-            <Editor onChange={setContent} value={content} />
-          </div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            style={{ marginTop: "5px" }}
-          >
-            Update post
-          </button>
-        </form>
+          </svg>
+          <rect
+            width="100%"
+            height="100%"
+            stroke-width="0"
+            fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
+          />
+        </svg>
       </div>
-    </main>
+      <div className="space-y-12 mx-24 my-10 bg-white bg-opacity-20 backdrop-blur-sm drop-shadow-[0_99px_90px_rgba(207,77,114,0.35)]">
+        <div className="border py-10 px-24 border-primary pb-12">
+          <h2 className=" text-3xl text-primary font-semibold leading-7 ">
+            UPDATE ME
+          </h2>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="flex flex-row gap-24">
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-primary"
+                >
+                  Username
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-xl  border border-primary mb-2  sm:max-w-md">
+                    <div>
+                      <input
+                        type="title"
+                        placeholder={"Title"}
+                        value={title}
+                        onChange={(ev) => setTitle(ev.target.value)}
+                        autoComplete="username"
+                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-600 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-primary"
+                >
+                  Summary
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md mb-2  border border-primary sm:max-w-md">
+                    <div>
+                      <textarea
+                        type="summary"
+                        placeholder={"Summary"}
+                        rows="3"
+                        cols="50"
+                        value={summary}
+                        onChange={(ev) => setSummary(ev.target.value)}
+                        autoComplete="Summary"
+                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-600 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-primary"
+                >
+                  Category
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md mb-2  border border-primary sm:max-w-md">
+                    <div>
+                      <input
+                        type="category"
+                        placeholder={"Category"}
+                        value={category}
+                        onChange={(ev) => setCategory(ev.target.value)}
+                        autoComplete="Category"
+                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-primary placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-full">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium leading-6 text-primary"
+              >
+                About
+              </label>
+              <Editor onChange={setContent} value={content} />
+
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                Write a few sentences about your receipe.
+              </p>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="cover-photo"
+                className="block text-sm font-medium leading-6 text-primary"
+              >
+                Recepie Picture
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-primary/25 px-6 py-10">
+                <div className="text-center">
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-primary"
+                    >
+                      <input
+                        type="file"
+                        onChange={(ev) => setFiles(ev.target.files)}
+                      />
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="submit"
+              className="rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+            >
+              Update post
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input
+        type="category"
+        placeholder={"Category"}
+        value={category}
+        onChange={(ev) => setCategory(ev.target.value)}
+      />
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
+      <Editor onChange={setContent} value={content} />
+      <button style={{ marginTop: "5px" }}>Update post</button> */}
+    </form>
   );
 }
